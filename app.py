9c4b290fcc080cc5d6c8210656c85fe71abe12f2
serranolash <serranolash@gmail.com>
2025-11-28 16:57:05 -0300

    abrir puertos

--- app.py
+++ app.py
@@ -32,3 +32,7 @@ def create_appointment():
     db.append(data)
     save_db(db)
     return jsonify({"status": "ok", "saved": data}), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
